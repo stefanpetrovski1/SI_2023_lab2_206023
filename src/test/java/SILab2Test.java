556766a8b34de3ed4dfaee4c67a45a1ff6acb50c
Stefan Petrovski <spetrovski09@gmail.com>
2023-05-31 21:46:46 +0200

    Added M.C. tests in SILab2Test class

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,2 +1,76 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SILab2Test {
+
+    // Multiple condition tests
+    @Test
+    public void userIsNullTest() {
+        RuntimeException ex;
+        User user = null;
+        List<User> users = null;
+
+        // user is null, others are anything
+        ex = Assertions.assertThrows(RuntimeException.class, () -> {
+            SILab2.function(user, users);
+        });
+        Assertions.assertEquals("Mandatory information missing!", ex.getMessage());
+
+
+        ex = Assertions.assertThrows(RuntimeException.class, () -> {
+            SILab2.function(user, users);
+        });
+
+        Assertions.assertEquals("Mandatory information missing!", ex.getMessage());
+
+
+        ex = Assertions.assertThrows(RuntimeException.class, () -> {
+            SILab2.function(user, users);
+        });
+
+        Assertions.assertEquals("Mandatory information missing!", ex.getMessage());
+    }
+
+    @Test
+    public void passwordIsNullTest() {
+        RuntimeException ex;
+        User user = new User("stefan206023finki", null, "anything");
+        ;
+        List<User> users = null;
+
+        // user is not null, password is null, email is anything
+        ex = Assertions.assertThrows(RuntimeException.class, () -> {
+            SILab2.function(user, users);
+        });
+        Assertions.assertEquals("Mandatory information missing!", ex.getMessage());
+    }
+
+    @Test
+    public void emailIsNullTest() {
+        RuntimeException ex;
+        User user = new User("stefan206023finki", "password", null);
+        ;
+        List<User> users = null;
+
+        // user is not null, password is not null, email is null
+        ex = Assertions.assertThrows(RuntimeException.class, () -> {
+            SILab2.function(user, users);
+        });
+        Assertions.assertEquals("Mandatory information missing!", ex.getMessage());
+    }
+
+    @Test
+    public void noExceptionTest() {
+
+        User user = new User("stefan206023finki", "password", "email");
+        List<User> users = null;
+
+        // user is not null, password is null, email is not null
+        Assertions.assertDoesNotThrow(() -> SILab2.function(user, users));
+    }
+
+
 }
